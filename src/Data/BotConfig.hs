d91b2e6dd598105f13945dc542447da665f4b3fa
Paul Ogris <paul@tsahyt.com>
2017-01-02 09:01:33 +0100

    Add JSON instance for config

--- src/Data/BotConfig.hs
+++ src/Data/BotConfig.hs
@@ -1,14 +1,21 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Data.BotConfig
 (
     BotConfig (..),
-    defaultConfig
+    defaultConfig,
+    readConfig
 )
 where
 
 import Network
 import Network.IRC
 import Data.Time.Clock
+import Data.Yaml
+import Data.ByteString.Char8 (ByteString, pack)
+import GHC.Generics
 
 data BotConfig = BotConfig
     { server    :: HostName
@@ -18,6 +25,18 @@ data BotConfig = BotConfig
     , adminUser :: UserName
     , rateTime  :: NominalDiffTime
     }
+    deriving (Show, Eq, Generic)
+
+instance FromJSON PortID where
+    parseJSON (Number s) = 
+        let x :: Int = truncate s 
+         in return . PortNumber . fromIntegral $ x
+    parseJSON _ = fail "type error in parsing PortID"
+
+instance FromJSON ByteString where
+    parseJSON = fmap pack . parseJSON
+
+instance FromJSON BotConfig
 
 defaultConfig :: BotConfig
 defaultConfig = BotConfig
@@ -28,3 +47,6 @@ defaultConfig = BotConfig
     , adminUser = "tsahyt"
     , rateTime  = 300
     }
+
+readConfig :: FilePath -> IO (Maybe BotConfig)
+readConfig = decodeFile
