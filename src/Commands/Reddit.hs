1973e5d0f2f45ae547bd209249199d64bc0ad815
Paul Ogris <paul@tsahyt.com>
2017-01-06 21:43:14 +0100

    Set user-agent for reddit API
    This now fails on parsing.

--- src/Commands/Reddit.hs
+++ src/Commands/Reddit.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Commands.Reddit
 (
@@ -19,21 +20,36 @@ import Data.Proxy
 import Data.Response
 import Network.HTTP.Client (Manager, newManager, defaultManagerSettings)
 import Network.HTTP.Client.TLS
+import Data.Yaml
+import qualified Data.Vector as V
 
 newtype SubReddit = SubReddit String
     deriving (Eq, Show, Ord, ToHttpApiData)
 
 type RedditRandom = "r" 
                  :> Capture "subreddit" SubReddit 
-                 :> "random" :> Get '[JSON] String
+                 :> Header "User-Agent" String
+                 :> "random" :> Get '[JSON] Reddit
 
-query :: SubReddit -> Manager -> BaseUrl -> ClientM String
+query :: SubReddit -> Maybe String -> Manager -> BaseUrl -> ClientM Reddit
 query = client (Proxy :: Proxy RedditRandom)
 
 baseUrl :: BaseUrl
 baseUrl = BaseUrl Http "api.reddit.com" 80 ""
 
+data Reddit = Reddit { url :: String }
+    deriving (Show, Eq, Ord)
+
+instance FromJSON Reddit where
+    parseJSON (Array v) = parseJSON (v V.! 0)
+    parseJSON (Object v) = do
+        --link <- v .: "link" 
+        url  <- v .: "url"
+        return $ Reddit url
+    parseJSON _ = error "Malformed reddit response"
+
 randomReddit :: MonadIO m => Manager -> SubReddit -> ByteString -> Response m
 randomReddit man sub cmd = simpleCmd cmd $ \_ chan -> do
-    res <- liftIO $ runExceptT (query sub man baseUrl)
+    let uagent = Just "linux:tsahyt/lmrbot:v0.1.0 (by /u/tsahyt)"
+    res <- liftIO $ runExceptT (query sub uagent man baseUrl)
     return undefined
