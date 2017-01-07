c260a07a5c63c50356018940d81dece0d012dda5
Paul Ogris <paul@tsahyt.com>
2017-01-07 17:53:36 +0100

    Implement fallback full API lookup for Wolfram

--- src/Commands/Wolfram.hs
+++ src/Commands/Wolfram.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Commands.Wolfram
 (
+    WAResult (..),
+    QueryResult (..),
+    Pod (..),
     wolfram
 )
 where
@@ -19,9 +24,11 @@ import Data.BotConfig
 import Data.Maybe
 import Data.Monoid
 import Data.Proxy
+import GHC.Generics
 import Data.Attoparsec.ByteString.Char8
 import Data.ByteString.Char8 (unpack, pack)
 import Network.HTTP.Client (Manager)
+import Data.Aeson
 
 newtype Query = Query String
     deriving (Eq, Show, Ord, ToHttpApiData)
@@ -37,8 +44,45 @@ type WolframShort = "v1"
                  :> QueryParam "i" Query 
                  :> Get '[PlainText] String
 
+data WAResult = WAResult { queryresult :: QueryResult }
+    deriving (Eq, Show, Ord, Generic)
+
+instance FromJSON WAResult
+
+data QueryResult = QueryResult
+    { success :: Bool
+    , pods :: [Pod]
+    }
+    deriving (Eq, Show, Ord, Generic)
+
+instance FromJSON QueryResult
+
+data Pod = Pod
+    { title     :: String
+    , subpods   :: Maybe [Pod]
+    , plaintext :: Maybe String
+    }
+    deriving (Eq, Show, Ord, Generic)
+
+instance FromJSON Pod
+
+type WolframFull = "v2"
+                :> "query"
+                :> QueryParam "appid" WolframAPIKey
+                :> QueryParam "input" Query
+                :> QueryParam "output" String
+                :> QueryParam "format" String
+                :> QueryParam "includepodid" String
+                :> Get '[PlainText] WAResult
+
+instance MimeUnrender PlainText WAResult where
+    mimeUnrender _ = eitherDecode
+
+type WolframAPI = WolframShort :<|> WolframFull
+
 shortAnswer :: Client WolframShort
-shortAnswer = client (Proxy :: Proxy WolframShort)
+longAnswer :: Client WolframFull
+shortAnswer :<|> longAnswer = client (Proxy :: Proxy WolframAPI)
 
 baseUrl :: BaseUrl
 baseUrl = BaseUrl Http "api.wolframalpha.com" 80 ""
@@ -49,7 +93,17 @@ wolfram man appid = fromMsgParser parser $ \p chan q -> do
     let u = fromMaybe "Dave" $ msgUser' p
     res <- liftIO $
         runExceptT (shortAnswer appid (Just q) man baseUrl)
-    return $ case res of
-        Left _ -> privmsg (fromMaybe "" chan) $
-                      "I'm sorry " <> u <> ", I'm afraid I can't do that."
-        Right r -> privmsg (fromMaybe "" chan) (pack r)
+    case res of
+        Left _ -> do
+            res' <- liftIO $ runExceptT (longAnswer appid (Just q) 
+                                                          (Just "json") 
+                                                          (Just "plaintext") 
+                                                          (Just "Result") 
+                                                          man baseUrl)
+            case res' of
+                Right (WAResult (QueryResult _ 
+                    [Pod _ (Just [Pod _ _ (Just r)]) _])) -> 
+                    return $ privmsg (fromMaybe "" chan) (pack r)
+                _ -> return $ privmsg (fromMaybe "" chan) $
+                    "I'm sorry " <> u <> ", I'm afraid I can't do that."
+        Right r -> return $ privmsg (fromMaybe "" chan) (pack r)
