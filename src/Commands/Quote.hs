a32d6f3521c0830f155cf805e751fdef87b1fe04
Paul Ogris <paul@tsahyt.com>
2017-08-23 22:14:15 +0200

    Add preliminary quote search implementation
    This is likely way too slow

--- src/Commands/Quote.hs
+++ src/Commands/Quote.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Commands.Quote
 (
@@ -14,13 +13,17 @@ module Commands.Quote
 where
 
 import Control.Applicative
+import Control.Monad
 import Control.Monad.Random.Class
 import Data.FileEmbed
 import Data.Response
 import Data.Monoid
 import Data.Maybe
+import Data.Array hiding (bounds)
+import Data.List (tails, inits, minimumBy)
+import Data.Ord (comparing)
 import Network.IRC
-import Data.Vector (Vector, (!))
+import Data.Vector (Vector)
 import Data.ByteString.Char8 (ByteString)
 import Data.Attoparsec.ByteString.Char8
 import qualified Data.Vector as V
@@ -29,17 +32,53 @@ import Text.Printf
 
 type Quote = ByteString
 
-qcmd :: ByteString -> Parser (Maybe Int)
-qcmd cmd = string cmd *> optional (space *> signed decimal)
+editDistance :: ByteString -> ByteString -> Int
+editDistance xs ys = table ! (m,n)
+    where (m,n) = (B.length xs, B.length ys)
+          x     = array (1,m) (zip [1..] $ B.unpack xs)
+          y     = array (1,n) (zip [1..] $ B.unpack ys)
+     
+          table :: Array (Int, Int) Int
+          table = array bnds [(ij, dist ij) | ij <- range bnds]
+          bnds  = ((0,0),(m,n))
+     
+          dist (0,j) = j
+          dist (i,0) = i
+          dist (i,j) = minimum 
+              [ table ! (i-1,j) + 1
+              , table ! (i,j-1) + 1
+              , if x ! i == y ! j 
+                then table ! (i-1,j-1) 
+                else 1 + table ! (i-1,j-1)
+              ]
+
+search :: ByteString -> [Quote] -> Maybe Int
+search _ [] = Nothing
+search pat xs = Just . fst . 
+    minimumBy (comparing 
+        (minimum . map (editDistance pat . B.pack) . (tails >=> inits) . snd)) 
+       . zip [0..] . map B.unpack $ xs
+
+data QuoteCmd
+    = RandomQuote
+    | Numbered Int
+    | Search ByteString
+
+qcmd :: ByteString -> Parser QuoteCmd
+qcmd cmd = string cmd *> go
+    where go = (Search <$> (space *> takeByteString))
+           <|> (Numbered <$> (space *> signed decimal))
+           <|> pure RandomQuote
 
 quote :: MonadRandom m => ByteString -> Vector Quote -> Response m
 quote cmd qs = fromMsgParser (qcmd cmd) $ \_ chan k -> do
     let bounds = V.length qs
     r <- case k of
-        Nothing -> getRandomR (0, pred bounds)
-        Just r' -> return (r' `mod` bounds)
+        RandomQuote -> getRandomR (0, pred bounds)
+        Numbered r' -> return (r' `mod` bounds)
+        Search pat  -> return . fromMaybe 0 $ search pat (V.toList qs)
     let pfix = printf "[%d/%d] " r (pred bounds)
-    return $ privmsg (fromMaybe "" chan) (B.pack pfix <> qs ! r)
+    return $ privmsg (fromMaybe "" chan) (B.pack pfix <> qs V.! r)
 
 rms :: MonadRandom m => Response m
 rms = quote ":rms" rmsQs
