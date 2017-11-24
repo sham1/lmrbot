30102ef669e289d3abc500ac5d802e8f30dd0d8e
Paul Ogris <paul@tsahyt.com>
2017-11-24 16:57:10 +0100

    Allow uppercase :CIA

--- src/Commands/CIA.hs
+++ src/Commands/CIA.hs
@@ -5,15 +5,20 @@ module Commands.CIA
 )
 where
 
+import Control.Applicative
 import Control.Monad.Random.Class
 import Data.Response
 import Data.Maybe
 import Data.Monoid
 import Network.IRC
+import Data.Attoparsec.ByteString.Char8 (Parser, string)
 import qualified Data.ByteString.Char8 as B
 
+parser :: Parser B.ByteString
+parser = string ":cia" <|> string ":CIA"
+
 cia :: MonadRandom m => Response m
-cia = simpleCmd ":cia" $ \_ chan -> do
+cia = fromMsgParser parser $ \_ chan _ -> do
     c <- getRandomR (1337 :: Int, 99999 :: Int)
     let ret = "This incident has been reported. Case #" <> B.pack (show c)
     return $ privmsg (fromMaybe "" chan) ret
